28fbd6899a7a9bc6c6bc81d01cba726a00c4945f
shakilla1 <shakilshifat65@gmail.com>
2022-09-29 17:32:10 +0600

    complete my details

--- src/components/Selected/Selected.js
+++ src/components/Selected/Selected.js
@@ -8,21 +8,17 @@ const Selected = (props) => {
     return (
         <div>
 
-            <h3>Add A Break</h3>
-            <div className='add-break'>
-                <p>10s</p>
-                <p>20s</p>
-                <p>30s</p>
-                <p>40s</p>
-                <p>50s</p>
-            </div>
+            <h1>thi sisi </h1>
             <h2>Exercise Details</h2>
             <div className='exercise-time'>
                 <h3>Exercise time{props.length}
                 </h3>
                 <span>00</span>seconds
             </div>
-
+            <div className='break-time'>
+                <h3>Break Time</h3>
+                <span>00</span>seconds
+            </div>
             <button className='activity-complete'>Activity Complete</button>
 
 
